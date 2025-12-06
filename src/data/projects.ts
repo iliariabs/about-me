10b82b8c131b32c4fac6195c751366dd3f9b73d5
Ilia Riabsev <46lidiay@gmail.com>
2025-12-06 14:44:21 +0200

    Updated projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,16 +1,24 @@
 import raylibBlackjackImg from '../assets/projectcarousel/raylibblackjack.png';
 import aboutMeImg from '../assets/projectcarousel/aboutme.png';
+import sofiatrafficmapImg from '../assets/projectcarousel/sofiatrafficmap.png';
 
 export const projects = [
   {
     id: 1,
-    title: "RaylibBlackJack",
+    title: "Sofia Bus & Trams Map",
+    category: "Interactive Map",
+    image: sofiatrafficmapImg,
+    link: "https://iliariabs.github.io/sofia-traffic-map/"
+  },
+  {
+    id: 2,
+    title: "Raylib BlackJack",
     category: "Low-level & Graphics",
     image: raylibBlackjackImg,
     link: "https://github.com/iliariabs/Raylib-Blackjack"
   },
   { 
-    id: 2,
+    id: 3,
     title: "About Me",
     category: "Landing Page",
     image: aboutMeImg,
